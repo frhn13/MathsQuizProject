68ab21bc86026b02cb97acabb0e8149c68da52f0
frhn16 <faroomirz@hotmail.com>
2025-03-04 00:21:57 +0000

    Started making form for user to select maths topics to study

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,7 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, IntegerField, BooleanField
+from wtforms.validators import DataRequired, NumberRange
 
 class AnswerForm(FlaskForm):
     answer = StringField(label="Answer: ", validators=[DataRequired()])
     submit = SubmitField(label="Submit Answer")
+
+class TopicsForm(FlaskForm):
+    operations = BooleanField(label="Operations")
+    decimals = BooleanField(label="Fraction and Decimals")
+    calculus = BooleanField(label="Calculus")
+    equations = BooleanField(label="Equations")
+    expressions = BooleanField(label="Expressions")
+    sequences = BooleanField(label="Sequences")
+    graphs = BooleanField(label="Graphs")
+    basic_shapes = BooleanField(label="Basic Shapes")
+    three_d_shapes = BooleanField(label="3D Shapes")
+    trigonometry = BooleanField(label="Trigonometry and Pythagoreas")
+    questions = IntegerField(label="Enter the number of questions to answers", validators=[DataRequired(), NumberRange(10, 50)])
+    difficulty = IntegerField(label="Enter the starting difficulty of the questions", validators=[DataRequired(), NumberRange(1, 5)])
+    submit = SubmitField(label="Submit Selection")
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,6 @@
 from quiz import app
 from flask import render_template, redirect, url_for, request, flash, session
-from quiz.forms import AnswerForm
+from quiz.forms import AnswerForm, TopicsForm
 from quiz.QuizCode.quizCode import operations_question_generation
 
 @app.route("/remove")
@@ -8,7 +8,19 @@ def remove_page():
     session.pop("final_question", None)
     session.pop("final_answer", None)
     session.pop("final_difficulty_weighting", None)
+    session.pop("current_difficulty", None)
+    session.pop("difficulty_range", None)
+    session.pop("score", None)
     return redirect(url_for("base_page"))
+
+@app.route("/quiz_selection", methods=["GET", "POST"])
+def quiz_selection():
+    form = TopicsForm()
+
+    if request.method == "POST":
+        pass
+
+    return render_template("quiz_selection.html", form=form)
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
@@ -30,6 +42,7 @@ def base_page():
 
     final_answer = session.get("final_answer")
     final_question = session.get("final_question")
+
     if request.method == "POST":
         answer = request.form.get("answer")
         try:
@@ -39,11 +52,12 @@ def base_page():
                 if answer == "True" or answer == "False":
                     pass
                 else:
-                    flash("Should have entered a True or False")
+                    flash("You must enter True or False. Try again", category="danger")
                     return redirect(url_for("base_page"))
         except Exception:
-            flash("Should have entered a number")
+            flash("You must enter a number. Try again.", category="danger")
             return redirect(url_for("base_page"))
+
         if answer == final_answer:
             session["score"]  += 1
             print("Correct")
@@ -62,6 +76,7 @@ def base_page():
                     session["current_difficulty"] -= 1
                     session["difficulty_range"] = 50
             flash(f"You got it wrong! The correct answer is {final_answer}", category="danger")
+
         session.pop("final_question", None)
         session.pop("final_answer", None)
         session.pop("final_difficulty_weighting", None)
@@ -69,4 +84,5 @@ def base_page():
         print(session.get("difficulty_range"))
         return redirect(url_for("base_page"))
     current_difficulty = session.get("current_difficulty")
-    return render_template("quiz.html", form=form, final_question=final_question, current_difficulty=current_difficulty)
+    score = session.get("score")
+    return render_template("quiz.html", form=form, final_question=final_question, current_difficulty=current_difficulty, score=score)
