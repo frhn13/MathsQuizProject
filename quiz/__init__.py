2e7ca8f78bbbfe48e280fa8fc01584222e4d3267
frhn16 <faroomirz@hotmail.com>
2025-05-06 17:28:13 +0100

    Added comments for webpages

--- quiz/__init__.py
+++ quiz/__init__.py
@@ -4,13 +4,13 @@ from flask_login import LoginManager
 
 from .QuizCode.topic_manager import question_topic_selection
 
-app = Flask(__name__)
+app = Flask(__name__) # Creates app for webpage
 
 app.config["SECRET_KEY"] = "VerySecretKey" # Needed to display the form
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///maths_quiz.db"
 db = SQLAlchemy(app) # Creates database
 login_manager = LoginManager(app)
-login_manager.login_view = "login_page"
+login_manager.login_view = "login_page" # Login do through login webpage
 login_manager.login_message_category = "warning"
 
 from quiz import views
